641f3a1e8573f776c6bdd179f86e3777f1d95292
quinndyer <qdyer00@gmail.com>
2023-08-13 17:07:51 -0400

    tracking aliens

--- global.gd
+++ global.gd
@@ -3,6 +3,6 @@ extends Node
 var distance = 0
 var police_chase = false
 var level = 0
-const goal_distances = [1000, 2000, 3000, 4000]
+const goal_distances = [4000, 4000, 4000, 4000]
 const backgrounds = ["Grass", "Desert"]
 const level_background = ["Grass", "Grass", "Desert", "Desert"]
